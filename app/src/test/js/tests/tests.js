0966f15fbd0138bebc18b63f79c19cebde4f197e
melhamin <mo.elhamin@gmail.com>
2022-03-30 19:39:42 +0300

    add tests

--- app/src/test/js/tests/tests.js
+++ app/src/test/js/tests/tests.js
@@ -11,14 +11,20 @@ describe("Covid 19 Survey App Test Suite", () => {
     await driver.init(config.options.capabilities);
   });
 
-  it("The send button is activated when all inputs are populated.", async () => {
+  it("Tests if the send button is activated when all inputs are populated.", async () => {
     try {
       await driver.elementById("com.example.covidsurvey:id/textInputEditNameSurname").sendKeys("TEST USERNAME");
 
       // select date of birth
       await driver
         .elementByXPath(
-          "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.ImageButton"
+          `
+          /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.
+          widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/
+          android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[2]/
+          android.widget.FrameLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/
+          android.widget.ImageButton
+        `
         )
         .click();
 
@@ -46,7 +52,7 @@ describe("Covid 19 Survey App Test Suite", () => {
     }
   });
 
-  it("The send button reacts to valid and invalid data (activates with valid and deactivates with invalid).", async () => {
+  it("Tests if the send button reacts to valid and invalid data (activates with valid and deactivates with invalid).", async () => {
     try {
       let touchAction = new wd.TouchAction(driver).longPress({ x: 0, y: 259 }).moveTo({ x: 0, y: 1000 }).release();
       await touchAction.perform();
@@ -55,7 +61,13 @@ describe("Covid 19 Survey App Test Suite", () => {
       // select date of birth
       await driver
         .elementByXPath(
-          "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.ImageButton"
+          `
+          /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.
+          widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/
+          android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[2]/
+          android.widget.FrameLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/
+          android.widget.ImageButton
+        `
         )
         .click();
 
@@ -90,6 +102,72 @@ describe("Covid 19 Survey App Test Suite", () => {
       const isEnabledWithValidDataAfterChange = await driver.elementById("com.example.covidsurvey:id/sendButton").isEnabled();
 
       expect(isEnabledWithInvalidData).to.equal(false) && expect(isEnabledWithValidDataAfterChange).to.equal(true);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  it("Tests if the selected dates are valid.", async () => {
+    try {
+      let touchAction = new wd.TouchAction(driver).longPress({ x: 0, y: 259 }).moveTo({ x: 0, y: 1000 }).release();
+      await touchAction.perform();
+
+      await driver
+        .elementByXPath(
+          `
+          /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.
+          widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/
+          android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[2]/
+          android.widget.FrameLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/
+          android.widget.ImageButton
+        `
+        )
+        .click();
+
+      await driver.waitForElementById("com.example.covidsurvey:id/confirm_button");
+      await driver.elementById("com.example.covidsurvey:id/confirm_button").click();
+
+      const selectedDate = await driver.elementByXPath(`
+          /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/
+          android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ScrollView/android.view.ViewGroup/
+          android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout[2]/
+          android.widget.FrameLayout/android.widget.ImageButton
+      `);
+
+      const validDate = "00000000-0000-012b-ffff-ffff00000020";
+      expect(selectedDate.value).to.not.equal("");
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  it("Tests if the erorr message is shown if names are invlaid (containt numbers or special chars)", async () => {
+    try {
+      let touchAction = new wd.TouchAction(driver).longPress({ x: 0, y: 259 }).moveTo({ x: 0, y: 1000 }).release();
+      await touchAction.perform();
+
+      // Populate the input with invalid name
+      await driver.elementById("com.example.covidsurvey:id/textInputEditNameSurname").sendKeys("INVALID USERNAME 1234");
+      // Get the error message for the input
+      const value = await driver.elementById("com.example.covidsurvey:id/textInputEditNameSurname").printError().text();
+      expect(value).to.equal("INVALID USERNAME 1234");
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  it("Tests if an item is properly selected on drop down menus.", async () => {
+    try {
+      let touchAction = new wd.TouchAction(driver).longPress({ x: 0, y: 259 }).moveTo({ x: 0, y: 1000 }).release();
+      await touchAction.perform();
+
+      // Open the dropdown menu
+      await driver.elementById("com.example.covidsurvey:id/autoCompleteTextViewGender").click();
+      // Select 'Male' option
+      await driver.elementById("com.example.covidsurvey:id/autoCompleteTextViewGender").sendKeys("Male");
+      const selectedOption = await driver.elementById("com.example.covidsurvey:id/autoCompleteTextViewGender").text();
+
+      expect(selectedOption).to.equal("Male");
     } catch (error) {
       console.log(error);
     }
